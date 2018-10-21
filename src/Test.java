a12b7e538d7a14995f35495f42e3ea52dd0d6244
wjsw1121ws <wnmihun@outlook.com>
2018-10-22 00:33:51 +0800

    test git

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @Description:
+ * @ClassName: Test
+ * @Auther: changchun_wu
+ * @Date: 2018/10/22 0:30
+ * @Version: 1.0
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
